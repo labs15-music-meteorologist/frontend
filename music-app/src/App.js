bd2f29ecfec9acc5449ed02d9cf541430bf87fbe
rushi444 <rushi44@gmail.com>
2019-08-20 13:52:09 -0700

    Redirect to Spotify Login working
    Co-authored-by: Rushi Arumalla <rushi44@gmail.com>
    Co-authored-by: Justin Chen <justiny2c@gmail.com>
    Co-authored-by: Ben Feole <bfeole@gmail.com>
    Co-authored-by: Daniel Starling <danielstarling4@gmail.com>
    Co-authored-by: Jean Lafontant <jrl47@yahoo.com

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import ApiRunner from './components/ApiRunner';
+import Auth from './components/Auth'; 
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <>
+      <Router>
         <div className='App'>Hello World!</div>
+        <Route exact path='/' component ={Auth} />
         <ApiRunner />
-      </>
+      </Router>
     );
   }
 }

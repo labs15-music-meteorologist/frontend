f12941ae58a1241376ace09d6c55505f17398474
Zurglots <bfeole@gmail.com>
2019-08-21 17:53:15 -0400

    Refactored gaid placement

--- music-app/src/index.js
+++ music-app/src/index.js
@@ -8,10 +8,15 @@ import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducers';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
+import ReactGA from 'react-ga';
+
+const trackingId = 'UA-145979080-2';
+ReactGA.initialize(trackingId);
 
 Sentry.init({
   dsn: 'https://d1fc669b08fb4d33b336f1b64a48ae5b@sentry.io/1537793',
 });
+
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(

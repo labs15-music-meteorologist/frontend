bb5ed9bbfae48841a91afc9266f87db64b284e93
rushi444 <rushi44@gmail.com>
2019-08-28 12:48:38 -0700

    Changed RedirectUri to an environment variable

--- music-app/src/components/Auth.js
+++ music-app/src/components/Auth.js
@@ -5,7 +5,7 @@ import { Paper, Grid } from '@material-ui/core';
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 const clientId = '256aebf9b04a4f5480a757f770864028'; // testing ENV
-const redirectUri = 'http://localhost:3000/'; // has to match exactly with spotify dashboard redirect uri
+const redirectUri = process.env.REACT_APP_REDIRECT_URL; // has to match exactly with spotify dashboard redirect uri
 const scopes = [
   'streaming',
   'user-read-currently-playing',

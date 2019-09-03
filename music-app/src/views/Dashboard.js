bb2b8f094566a15846bb1ecbcf6e86137aea1f2a
justiny2c <justiny2c@gmail.com>
2019-09-03 13:33:22 -0700

    Corrected dashboard folder name

--- music-app/src/views/Dashboard.js
+++ music-app/src/views/Dashboard.js
@@ -5,23 +5,24 @@ import { Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import { makeStyles } from '@material-ui/core/styles';
 
 import { getlikedSongs, getUsers, getSpotifyAccountDetails } from '../actions';
-
 import LikedSongs from '../components/dashboard/LikedSongs';
 import MusicPlayer from '../components/dashboard/MusicPlayer';
-
 import '../App.css';
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    padding: theme.spacing(1),
-    backgroundColor: `rgba(${255}, ${189}, ${89}, ${0.75})`,
-  },
-}));
-
 class Dashboard extends React.Component {
+  state = {
+    collapse: false,
+  };
+
+  onClick() {
+    console.log(this.state.collapse);
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
   logout = e => {
     e.preventDefault();
     localStorage.removeItem('token');
@@ -49,31 +50,72 @@ class Dashboard extends React.Component {
     }
     return (
       <div className='dashboard'>
-        <Button
-          variant='contained'
-          className={useStyles.button}
-          onClick={e => this.logout(e)}>
-          Logout
-        </Button>
-        <Grid
-          container
-          direction='row'
-          justify='space-around'
-          alignItems='center'>
-          <Grid item>
+        <div>
+          <Button
+            className='burger'
+            style={{ color: 'white' }}
+            onClick={() => this.onClick()}>
+            <Grid container direction='column'>
+              <div
+                className={
+                  this.state.collapse
+                    ? 'line-one mobile-only toggle-line-one'
+                    : 'line-one mobile-only'
+                }
+              />
+              <div
+                className={
+                  this.state.collapse
+                    ? 'line-two mobile-only toggle-line-two'
+                    : 'line-two mobile-only'
+                }
+              />
+              <div
+                className={
+                  this.state.collapse
+                    ? 'line-three mobile-only toggle-line-three'
+                    : 'line-three mobile-only'
+                }
+              />
+            </Grid>
+            Playlist
+          </Button>
+          <Button
+            variant='contained'
+            onClick={e => this.logout(e)}
+            style={{
+              backgroundColor: `rgba(${255}, ${189}, ${89}, ${0.75})`,
+              fontWeight: '600',
+              letterSpacing: '1.5px',
+              position: 'absolute',
+              right: '0px',
+              marginRight: '5%',
+              marginTop: '0.8%',
+            }}>
+            Logout
+          </Button>
+          <List>
             <Paper
+              className={
+                this.state.collapse ? 'side-menu-open' : 'side-menu-closed'
+              }
               style={{
                 maxHeight: 450,
-                width: 380,
+                width: 280,
                 overflow: 'auto',
                 backgroundColor: `rgba(${20}, ${20}, ${20}, ${0.85})`,
                 color: 'white',
               }}>
-              <List>
-                <LikedSongs props={this.props} />
-              </List>
+              <LikedSongs props={this.props} />
             </Paper>
-          </Grid>
+          </List>
+        </div>
+        <Grid
+          className='dashboard-grid'
+          container
+          direction='row'
+          justify='space-around'
+          alignItems='center'>
           <Grid item>
             <MusicPlayer />
           </Grid>

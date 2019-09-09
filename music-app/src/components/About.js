f7abce18d960c4454fb1b3459009134a62080e52
jrl508 <jrl47@yahoo.com>
2019-09-09 13:46:49 -0400

    initial commit

---
+++ music-app/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => (
+  <>
+    <div>
+      <h1>About</h1>
+      <p>
+        Music Meteorologist aims to be the audiophpile's guilty pleasure, a
+        project rooted out of Lambda School's Labs that works off of a user's
+        Spotify account (currently Premium users only)
+      </p>
+    </div>
+  </>
+);
+
+export default About;

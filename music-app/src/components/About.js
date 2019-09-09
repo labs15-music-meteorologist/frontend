c89e2a72398cfae898ce59ffc3bf66edb6b7bee5
jrl508 <jrl47@yahoo.com>
2019-09-09 15:36:41 -0400

    fresh pull

--- music-app/src/components/About.js
+++ music-app/src/components/About.js
@@ -7,7 +7,8 @@ const About = () => (
       <p>
         Music Meteorologist aims to be the audiophpile's guilty pleasure, a
         project rooted out of Lambda School's Labs that works off of a user's
-        Spotify account (currently Premium users only)
+        Spotify account (currently Premium users only) that allows the user to
+        really shape what their player reccomends
       </p>
     </div>
   </>

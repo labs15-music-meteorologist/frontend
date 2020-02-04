a9541bafcb92a4eeca73a71c2538254f7a92f018
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-04 10:22:35 -0800

    refactored Album info into own component.

--- music-app/src/components/dashboard/AlbumInfo.component.jsx
+++ music-app/src/components/dashboard/AlbumInfo.component.jsx
@@ -1,41 +1,29 @@
-import React from "react";
-import { connect } from "react-redux";
-import { saveLikedSong, removeTrack } from "../../Redux/Spotify/spotify.actions";
+import React, { useRef } from "react";
+import { Grid } from "@material-ui/core";
 
 const AlbumInfo = props => {
-  const {  } = props;
+  const { imageSpotify, trackName, artistName, albumName } = props;
 
-  
   return (
     <div className='music-component'>
-    <Grid
-      item
-      className='music-component-album-info'
-      style={{ maxWidth: "300px" }}
-    >
-      {/* {this.props.imageUrl[1] && ( */}
-      <img
-        ref='image'
-        src={imageSpotify}
-        alt='Album artwork cover.'
-        style={{ maxWidth: "300px", objectFit: "scale-down" }}
-      />
-      {/* )} */}
-      <p className='p' style={{ fontWeight: "bold" }}>
-        {trackName}
-      </p>
-      <p>{artistName}</p>
-      <p>{albumName}</p>
-    </Grid>
-  </div>
+      <Grid
+        item
+        className='music-component-album-info'
+        style={{ maxWidth: "300px" }}
+      >
+        <img
+          src={imageSpotify}
+          alt='Album artwork cover.'
+          style={{ maxWidth: "300px", objectFit: "scale-down" }}
+        />
+        <p className='p' style={{ fontWeight: "bold" }}>
+          {trackName}
+        </p>
+        <p>{artistName}</p>
+        <p>{albumName}</p>
+      </Grid>
+    </div>
   );
 };
 
-const mapStateToProps = state => ({
-  song: state.currentSongReducer.item,
-  currentUser: state.getCurrentUserReducer.currentUser
-});
-
-export default connect(mapStateToProps, { saveLikedSong, removeTrack })(
-  LikeAndDislikeButton
-);
+export default AlbumInfo;

4ebe833f0bf79da231ba6e0df24fe6bd289eee87
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-17 14:38:59 -0800

    Revert "Changed BrowserRouter to outside of App scope instead of inner."
    This reverts commit 5dacfd9b61852d40dd36d2b412c8c1ce999e6221.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
-import React, { Component } from "react";
-import { ProtectedRoute } from "./components/protected.route";
-import { Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { ProtectedRoute } from './components/protected.route';
 
-import Auth from "./components/Auth";
-import MusicPlayer from "./components/dashboard/MusicPlayer";
-import FooterContainer from "./components/Footer.js";
+import ReactGA from 'react-ga';
 
-import Logout from "./views/Logout";
-import Info from "./views/Info";
-import Dashboard from "./views/Dashboard.js";
-import "./App.css";
-import About from "./components/About";
+import Auth from './components/Auth';
+import MusicPlayer from './components/dashboard/MusicPlayer';
+import FooterContainer from './components/Footer.js';
+
+import Logout from './views/Logout';
+import Info from './views/Info';
+import Dashboard from './views/Dashboard.js';
+import './App.css';
+import About from './components/About';
 
 /* import LikedSongs from './components/LikedSongs'; */
+
+const history = createBrowserHistory();
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Route exact path='/' render={props => <Auth {...props} />} />
-        <Route exact path='/logout' component={Logout} />
-        <Route exact path='/info' render={props => <Info {...props} />} />
-        <Route exact path='/player' component={MusicPlayer} />
-        <Route exact path='/about' component={About} />
-        <ProtectedRoute exact path='/dashboard' component={Dashboard} />
+        <Router history={history}>
+          <Route exact path='/' render={props => <Auth {...props} />} />
+          <Route exact path='/logout' component={Logout} />
+          <Route exact path='/info' render={props => <Info {...props} />} />
+          <Route exact path='/player' component={MusicPlayer} />
+          <Route exact path='/about' component={About} />
+          <ProtectedRoute exact path='/dashboard' component={Dashboard} />
+        </Router>
         {/* <FooterContainer /> */}
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,40 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createBrowserHistory } from "history";
-import { BrowserRouter as Router } from "react-router-dom";
-import { logger } from "redux-logger";
-import thunk from "redux-thunk";
-import { applyMiddleware, createStore } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
 
-import * as Sentry from "@sentry/browser";
-import ReactGA from "react-ga";
+import * as Sentry from '@sentry/browser';
+import ReactGA from 'react-ga';
 
-import rootReducer from "./Redux/reducers";
-import "./index.css";
-import App from "./App";
+import rootReducer from './Redux/reducers';
+import './index.css';
+import App from './App';
 
 // import 'typeface-roboto';
 
-ReactGA.initialize("UA-145979080-2");
+ReactGA.initialize('UA-145979080-2');
 
 Sentry.init({
-  dsn: "https://d1fc669b08fb4d33b336f1b64a48ae5b@sentry.io/1537793"
+  dsn: 'https://d1fc669b08fb4d33b336f1b64a48ae5b@sentry.io/1537793',
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
-const history = createBrowserHistory();
-
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
       <App />
-    </Router>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );

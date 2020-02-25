3b24ccb57e74ffa77483e0618a9f1511da159423
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-02-25 13:52:12 -0500

    added loader styles containter in loader file js

--- src/views/LoadingPage.js
+++ src/views/LoadingPage.js
@@ -3,16 +3,32 @@ import styled from 'styled-components';
 import '../App.css';
 import Loader from '../assets/loader.gif';
 
+const LoadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  height: 600px;
+  width: 600px;
+  padding-top: 14%;
+  margin: 0%;
+  @media (max-width: 576px) {
+    padding-top: 45%;
+    margin: 2%;
+  }
+`;
 
-const FooterContainer = () => {
+const LoaderContainer = () => {
 
   return (
     <div>
-      chill, its coming
+      <LoadContainer>
+        {/* chill, its coming */}
         <img src={Loader} />
+      </LoadContainer>
     </div>
   );
 
 }
 
-export default FooterContainer
+export default LoaderContainer
